2a7f963481e3dfcaf088a4f463b1642c1d93d563
p0kexgames <30707863+p0kexgames@users.noreply.github.com>
2017-11-30 01:29:57 -0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7425,7 +7425,7 @@ criarPaginacaodeTabela();
 
 
  // alert ('Atenção, \n\nPara ver o local no mapa clique na imagem do pokemon, em seguida selecione a aba mapas clique na imagem em miniatura para abrir a imagem na tela grande e repare nos marcadores em vermelho\n\n\nCaso os locais destacados estejam errados deixe seu comentario e se possível uma print da sua tela para que nos consertemos.');
-  alert ('Já é possível visualizar os pokes Shinys, Moves, Loots (de todos os pokes) e tambêm as médias (dos shiny cyber)\n\nPara ver as informações do pokemon shiny basta clicar na foto do pokemon desejado, na janela que abrir posicioneo mouse em cima da bolinha verde (ao lado da foto) que irá mostrar as versões shinys, megas disponíveis.\n\n\nEstamos atualizando as informações, contamos com sua colaboração caso encontre algum erro.');
+  alert ('Olá Já é possível visualizar os pokes Shinys, Moves, Loots (de todos os pokes) e tambêm as médias (dos shiny cyber)\n\nPara ver as informações do pokemon shiny basta clicar na foto do pokemon desejado, na janela que abrir posicioneo mouse em cima da bolinha verde (ao lado da foto) que irá mostrar as versões shinys, megas disponíveis.\n\n\nEstamos atualizando as informações, contamos com sua colaboração caso encontre algum erro.');
 
 // ATUALIZA O MODAL DE INFORMAÇÃO DO POKE AO CLICAR NA CLASSE .ITEM
  $(document).on("click", ".item", function () {
@@ -10253,6 +10253,12 @@ jQuery(document).ready(function($){
      pesquisaavancada();
   });
 
+  $(document).on("click", ".item3", function () {
+     var mySearch = $(this).find('h4').text();
+     ga('send', 'event', 'Clicks', 'Shiny', mySearch);
+     alert(mySearch);
+  });
+ 
 
 
 
@@ -10473,13 +10479,6 @@ $(".tab-forca a").click( function() {
     });
 
 
- 
-    $('.item3').click(function() {
-     //var mySearch = $('.modal h4 .nome').html();
-     var mySearch = $(this).find('.nome').text();
-     ga('send', 'event', 'Clicks', 'Shiny', mySearch);
-     alert(mySearch);
-    });
  
     $('.card.card-clan .card-block a').click(function() {
      var mySearch = $(this).find('i').text();
