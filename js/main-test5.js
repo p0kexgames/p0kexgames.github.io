0153716d9462612900a51f954da47f58077fdde6
p0kexgames <30707863+p0kexgames@users.noreply.github.com>
2017-12-15 17:59:32 -0200

    Update main-test5.js

--- js/main-test5.js
+++ js/main-test5.js
@@ -8554,7 +8554,8 @@ $(".aviso").html(" ");
 
    
    $(".modal-header b.nome").html( myPokeNome );
-   $(".modal-header b.numero").html( myPokeNum );
+   //$(".modal-header b.numero").html( myPokeNum );
+   $(".modal-header b.numero").html(myPokeNum.split('-')[0].slice(0, -1));
    
    $(".modal-body .modal-capa .pokedex").attr('class', 'col-xs-12 col-sm-12 col-md-12 col-lg-12 pokedex');
    $(".modal-body .modal-capa .pokedex").addClass('pokedex-'+myPokeNum);
@@ -10241,7 +10242,8 @@ $(".aviso").html(" ");
 
    
    $(".modal-header b.nome").html( myPokeNome );
-   $(".modal-header b.numero").html( myPokeNum );
+   //$(".modal-header b.numero").html( myPokeNum );
+   $(".modal-header b.numero").html(myPokeNum.split('-')[0].slice(0, -1));
    
    $(".modal-body .modal-capa .pokedex").attr('class', 'col-xs-12 col-sm-12 col-md-12 col-lg-12 pokedex');
    $(".modal-body .modal-capa .pokedex").addClass('pokedex-'+myPokeNum);
