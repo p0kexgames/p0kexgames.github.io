cb4711ac10f92b81d4b3c0b32573ef022a56efbe
p0kexgames <30707863+p0kexgames@users.noreply.github.com>
2018-03-26 20:41:21 -0300

    Update main-test5.js

--- js/main-test5.js
+++ js/main-test5.js
@@ -8223,6 +8223,16 @@ $(".aviso").html(" ");
        $(".aviso").addClass( 'show' );
        $(".aviso").removeClass( 'hide' );
   }
+  if (myPokeDificuldade === 'Quest') {
+       $(".modal").addClass('Quest');
+       $(".Quest .ballstable div").html(" ");
+       text = "<div class='dificuldade'>";
+        text += "<i class='fa fa-exclamation-circle'></i> Esse Pokemon poderá ser adquirido através de quest.";
+       text += "</div>";
+       $(".aviso").append( text );
+       $(".aviso").addClass( 'show' );
+       $(".aviso").removeClass( 'hide' );
+  }
   if (myPokeDificuldade === 'Form') {
        $(".modal").addClass('Form');
        $(".Form .ballstable div").html(" ");
