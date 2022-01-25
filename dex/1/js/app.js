6ef7b3339f47ec52ea188884bbcf26f263ce9309
p0kexgames <30707863+p0kexgames@users.noreply.github.com>
2022-01-25 01:11:23 -0300

    Add files via upload

--- dex/1/js/app.js
+++ dex/1/js/app.js
@@ -1,7 +1,8 @@
 let warning
 let pokedexChilds
-let pokedexValues
+let pokedexValuesf
 let pokemonTypes = []
+let pokemonClans = []
 const pokedexElement = document.querySelector('.pokedex')
 const filterName = document.querySelector('#filter-name')
 const filterType = document.querySelector('#filter-type')
@@ -88,26 +89,41 @@ function pokemonCard(pokemon) {
   const types = pokemon.type
     .map(t => `<span class="pokemon-type background-${t}">${t}</span>`)
     .join('')
+  const clans = pokemon.clan
+    .map(t => `<span class="pokemon-clan background-${t}">${t}</span>`)
+    .join('')
   const img = pokemon.name.replace(/['\.]/g, '').replace(/\s/g, '-')
   pokemonTypes = pokemonTypes.concat(pokemon.type)
-  return `<div class="pokemon" data-name="${pokemon.name}" data-type="${pokemon.type}" tabindex="${pokemon.id}">
+  pokemonClans = pokemonClans.concat(pokemon.clan)
+  return `<div class="pokemon" data-name="${pokemon.name}" data-type="${pokemon.type}" data-clan="${pokemon.clan}" tabindex="${pokemon.id}">
       <figure class="pokemon-figure">
         <img src="img/${img.toLowerCase()}.png" alt="${pokemon.name}">
       </figure>
       <section class="pokemon-description">
         <span class="pokemon-id">#${Number(pokemon.id).toString().padStart(3, '0')}</span>
         <h1 class="pokemon-name">${pokemon.name}</h1>
+        <h1 class="pokemon-level">Level: ${pokemon.level}</h1>
         <div class="pokemon-types">${types}</div>
+        <div class="pokemon-clans">${clans}</div>
       </section>
       <section class="pokemon-stats">${loadStats(pokemon.stats)}</section>
-      <section class="pokemon-moves">${loadMoves(pokemon.moves)}</section>
+      <section class="pokemon-description2">
+        <span class="pokemon-price">Price: $${Number(pokemon.price).toString().padStart(3, '0')}</span>
+        <span class="pokemon-xp">XP: ${Number(pokemon.xp).toString().padStart(3, '0')}</span>
+        <div class="container">
+          <div class="dif-lvl-out">
+            <div class="dif-lvl lvl${pokemon.dificulty}">${pokemon.dificulty}</div>
+          </div>
+        </div>
+          
+      </section>
     </div>`
-}
+} 
 
 function loadTypeFilter(types) {
   types.map(t => filterType.insertAdjacentHTML('beforeend', `<option>${t}</option>`))
 }
-
+ 
 function loadStats(stats) {
   return Object.entries(stats)
     .filter(([name, value]) => !['total'].includes(name))
@@ -120,18 +136,18 @@ function loadStats(stats) {
       </div>`
     )
     .join('')
-}
+} 
 
-function loadMoves(moves) {
-  return Object.entries(moves)
-    .map(([name, value]) =>
-      `<div class="move-row">
-        <div>${name}</div>
-        <div>${value}</div>
+function loadDificulty(dificulty) {
+  return 
+      `<div class="stat-row">
+        <div class="stat-bar">
+          <div class="stat-bar-bg" style="width: ${100*value/250}%">${value}</div>
+        </div>
       </div>`
-    )
-    .join('')
-}
+    
+} 
+
 
 Array.prototype.uniq = function () {
   return this.filter(function (value, index, self) {
